e4f9906b49d5d6ad2e9cacbf82e6db86680ac5df
Sushil Khadka <punksusil.khadka@gmail.com>
2022-05-19 17:52:19 +0545

    Created Custom User model
    Updated precommit
    Created a setup.cfg file for python and flake8 config
    Customized admin panel of User

---
+++ accounts/models.py
@@ -0,0 +1,77 @@
+from django.apps import apps
+from django.contrib import auth
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, phone, email, password, **extra_fields):
+        """
+        Create and save a user with the given phone, email, and password.
+        """
+        if not phone:
+            raise ValueError("The given phone must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        _ = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
+        user = self.model(phone=phone, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(phone, email, password, **extra_fields)
+
+    def create_superuser(self, phone, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(phone, email, password, **extra_fields)
+
+    def with_perm(
+        self, perm, is_active=True, include_superusers=True, backend=None, obj=None
+    ):
+        if backend is None:
+            backends = auth._get_backends(return_tuples=True)
+            if len(backends) == 1:
+                backend, _ = backends[0]
+            else:
+                raise ValueError(
+                    "You have multiple authentication backends configured and "
+                    "therefore must provide the `backend` argument."
+                )
+        elif not isinstance(backend, str):
+            raise TypeError(
+                "backend must be a dotted import path string (got %r)." % backend
+            )
+        else:
+            backend = auth.load_backend(backend)
+        if hasattr(backend, "with_perm"):
+            return backend.with_perm(
+                perm,
+                is_active=is_active,
+                include_superusers=include_superusers,
+                obj=obj,
+            )
+        return self.none()
+
+
+class User(AbstractUser):
+    pass
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.phone}({self.get_full_name()})"
