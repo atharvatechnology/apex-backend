79b6503e04c4880318652e4d12b46fcac5cce2a1
sabina489 <Karkisabina869@gmail.com>
2022-05-25 15:39:06 +0545

    Create serializer class.

---
+++ courses/api/serializers.py
@@ -0,0 +1,104 @@
+# from pyexpat import model
+from rest_framework import serializers
+
+from courses.models import Course, CourseCategory
+
+
+class CourseCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = (
+            "id",
+            "name",
+            "link",
+            "password",
+            "status",
+            "price",
+        )
+
+
+class CourseRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = (
+            "id",
+            "name",
+            "link",
+            "password",
+            "status",
+            "price",
+        )
+        # extra_kwargs = {'password':{
+        #     'write_only': True
+        # }}
+
+        # def create(self, validated_data):
+        #     course = Course.objects.create(**validated_data)
+        #     course.save()
+        #     return course
+
+
+class CourseUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = (
+            "id",
+            "name",
+            "link",
+            "password",
+            "status",
+            "price",
+        )
+
+
+class CourseDeleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = (
+            "id",
+            "name",
+            "link",
+            "password",
+            "status",
+            "price",
+        )
+
+
+class CourseCategoryCreateSerialilzer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseCategory
+        fields = (
+            "id",
+            "name",
+            "description",
+        )
+
+
+class CourseCategoryRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseCategory
+        fields = (
+            "id",
+            "name",
+            "description",
+        )
+
+
+class CourseCategoryUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseCategory
+        fields = (
+            "id",
+            "name",
+            "description",
+        )
+
+
+class CourseCategoryDeleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseCategory
+        fields = (
+            "id",
+            "name",
+            "description",
+        )
