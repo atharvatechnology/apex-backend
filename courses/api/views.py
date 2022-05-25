f7f0f24c50a97d299dd165874d691dc977648e6c
sabina489 <Karkisabina869@gmail.com>
2022-05-25 15:44:59 +0545

    Create CRUD for course models.

---
+++ courses/api/views.py
@@ -0,0 +1,88 @@
+from rest_framework import filters
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
+from rest_framework.permissions import AllowAny
+
+from courses.api.serializers import (
+    CourseCategoryCreateSerialilzer,
+    CourseCategoryDeleteSerializer,
+    CourseCategoryRetrieveSerializer,
+    CourseCategoryUpdateSerializer,
+    CourseCreateSerializer,
+    CourseDeleteSerializer,
+    CourseRetrieveSerializer,
+    CourseUpdateSerializer,
+)
+from courses.models import Course, CourseCategory
+
+
+class CourseListAPIView(ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseRetrieveSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name"]
+    queryset = Course.objects.all()
+
+
+class CourseCreateAPIView(CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseCreateSerializer
+
+
+class CourseRetrieveAPIView(RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseRetrieveSerializer
+    queryset = Course.objects.all()
+
+
+class CourseUpdateAPIView(UpdateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseUpdateSerializer
+
+    queryset = Course.objects.all()
+    # category_id = self.kwargs.get('catid')
+    # if category_id:
+    #     return queryset.filter(category=category_id)
+    # return queryset
+
+
+class CourseDeleteAPIView(DestroyAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseDeleteSerializer
+    queryset = Course.objects.all()
+
+
+class CourseCategoryListAPIView(ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseCategoryRetrieveSerializer
+    queryset = CourseCategory.objects.all()
+    # filter_backends = [filters.SearchFilter]
+    # search_fields = ['name']
+
+
+class CourseCategoryCreateAPIView(CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseCategoryCreateSerialilzer
+
+
+class CourseCategoryRetrieveAPIView(RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseCategoryRetrieveSerializer
+    queryset = CourseCategory.objects.all()
+
+
+class CourseCategoryUpdateAPIView(UpdateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseCategoryUpdateSerializer
+    queryset = CourseCategory.objects.all()
+
+
+class CourseCategoryDeleteAPIView(DestroyAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = CourseCategoryDeleteSerializer
+    queryset = CourseCategory.objects.all()
