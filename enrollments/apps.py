03d85805f4638f28ef9ed157318313d18324f7f9
shovan777 <raj.shrestha778@gmail.com>
2022-05-30 19:04:40 +0545

    Signal score calculation on exam attempt. Add apps to flake8 exclusion due to f401.

--- enrollments/apps.py
+++ enrollments/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EnrollmentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "enrollments"
+
+    def ready(self):
+        from . import signals
