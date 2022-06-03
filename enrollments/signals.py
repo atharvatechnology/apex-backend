c02e926f6b4e7d17be339536cf9627a0cff34bca
shovan777 <raj.shrestha778@gmail.com>
2022-06-03 17:07:55 +0545

    Signal exam state transition based on session state.

--- enrollments/signals.py
+++ enrollments/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import ExamEnrollmentStatus
+from .models import ExamEnrollmentStatus, SessionStatus
 from .tasks import calculate_score
 
 
@@ -9,3 +9,23 @@ from .tasks import calculate_score
 def on_exam_attempt(sender, instance, **kwargs):
     if instance.status == ExamEnrollmentStatus.ATTEMPTED:
         calculate_score.delay(instance.id)
+
+
+@receiver(post_save, sender="enrollments.Session")
+def on_exam_session_save(sender, instance, **kwargs):
+    if instance.status == SessionStatus.INACTIVE:
+        print("session inactive")
+        instance.exam.schedule_exam()
+    if instance.status == SessionStatus.ACTIVE:
+        print("session active")
+        instance.exam.start_exam()
+    if instance.status == SessionStatus.ENDED:
+        print("session ended")
+        instance.exam.finish_exam()
+        # prevent further enrollment into that session
+        # prevent further submissions into that ExamEnrollment
+        # start the calculation of the score
+        for exam_through_enrollment in instance.session_enrolls.all():
+            exam_through_enrollment.attempt_exam()
+            # scoring calculation automatically beigns after
+            # exam is attempted
