711e85c57715ba973c9fd08300cfce101ee7b852
shovan777 <raj.shrestha778@gmail.com>
2022-06-08 12:17:41 +0545

    Broadcast exam status after exam start.

--- enrollments/signals.py
+++ enrollments/signals.py
@@ -1,8 +1,14 @@
+from asgiref.sync import async_to_sync
+from celery.utils.log import get_task_logger
+from channels.layers import get_channel_layer
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import ExamEnrollmentStatus, SessionStatus
 from .tasks import calculate_score
+
+channel_layer = get_channel_layer()
+logger = get_task_logger("__name__")
 
 
 @receiver(post_save, sender="enrollments.ExamThroughEnrollment")
@@ -16,9 +22,14 @@ def on_exam_session_save(sender, instance, **kwargs):
     if instance.status == SessionStatus.INACTIVE:
         print("session inactive")
         instance.exam.schedule_exam()
+
     if instance.status == SessionStatus.ACTIVE:
         print("session active")
         instance.exam.start_exam()
+        async_to_sync(channel_layer.group_send)(
+            "clock", {"type": "get_exam", "status": instance.exam.status}
+        )
+
     if instance.status == SessionStatus.ENDED:
         print("session ended")
         instance.exam.finish_exam()
