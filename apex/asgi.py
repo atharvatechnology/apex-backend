01380fa3a202f1da7923c9a75a9d2b5471b59c58
shovan777 <raj.shrestha778@gmail.com>
2022-06-11 11:17:48 +0545

    Load asgi application for the settings.

--- apex/asgi.py
+++ apex/asgi.py
@@ -10,6 +10,8 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 
 import clock.routing
 
@@ -18,13 +20,16 @@ import clock.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apex.settings")
 
+django_asgi_app = get_asgi_application()
+
+
 application = ProtocolTypeRouter(
     {
         # (http->django:)
         # "http": get_asgi_application(),
         # (websocket->django:)
-        "websocket": AuthMiddlewareStack(
-            URLRouter(clock.routing.websocket_urlpatterns)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(clock.routing.websocket_urlpatterns))
         ),
     }
 )
