8f5af884d69c65b8af736a9966f18cd87e42b317
sabina489 <Karkisabina869@gmail.com>
2022-06-18 08:12:44 +0545

    Add paginations to attendance models.

---
+++ attendance/api/paginations.py
@@ -0,0 +1,28 @@
+from rest_framework import pagination
+from rest_framework.response import responses
+
+
+class LargeResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 1000
+    page_size_query_param = "page_size"
+    max_page_size = 10000
+
+
+class StandardResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 100
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+
+class CustomPagination(pagination.PageNumberPagination):
+    def get_paginated_response(self, data):
+        return responses.Response(
+            {
+                "links": {
+                    "next": self.get_next_link(),
+                    "previous": self.get_previous_link(),
+                },
+                "count": self.page.paginator.count,
+                "results": data,
+            }
+        )
