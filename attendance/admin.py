d69df9bcc1a869ef2fc633d3fc1abbfbc29a691c
sabina489 <Karkisabina869@gmail.com>
2022-06-27 21:30:53 +0545

    Create admin for attendance

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from attendance.models import (
+from attendance.models import (  # Attendance,
     StudentAttendance,
     TeacherAttendance,
     TeacherAttendanceDetail,
