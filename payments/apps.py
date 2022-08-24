60740a7a7359fbbdd16d35e73f2cc1bd69a8ff4e
tyagisen <tyagisen@gmail.com>
2022-08-24 09:38:22 +0545

    Add Signals to change EnrollmentStatus after payment

--- payments/apps.py
+++ payments/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PaymentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "payments"
+
+    def ready(self):
+        import payments.signals
