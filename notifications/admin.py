6fb8d9ba666713bb8c35d1061473cdb186b93460
Sushil Khadka <punksusil.khadka@gmail.com>
2022-09-13 17:10:51 +0545

    Create Model to store Notification message

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,1 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from notifications.models import NotificationMessage
+
+
+@admin.register(NotificationMessage)
+class NotificationMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "body",
+        "created_at",
+    )
+    date_hierarchy = "created_at"
--- notifications/models.py
+++ notifications/models.py
@@ -1,1 +1,7 @@
-# from django.db import models
+from django.db import models
+
+
+class NotificationMessage(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
