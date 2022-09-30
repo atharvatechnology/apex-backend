54ffb588786abc36b6b60d7ff8df46e3f89f5a18
Sushil Khadka <punksusil.khadka@gmail.com>
2022-09-30 09:46:17 +0545

    Notification list and Notification message params

--- notifications/api_admin/views.py
+++ notifications/api_admin/views.py
@@ -1,6 +1,6 @@
 from fcm_django.models import FCMDevice
-from firebase_admin.messaging import Message, Notification
-from rest_framework.generics import CreateAPIView
+from firebase_admin.messaging import APNSConfig, APNSPayload, Aps, Message, Notification
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAdminUser
 
 from notifications.api_admin.serializers import NotificationSerializer
@@ -14,5 +14,23 @@ class SendPushNotification(CreateAPIView):
 
     def perform_create(self, serializer):
         super().perform_create(serializer)
-        message_obj = Message(notification=Notification(**serializer.data))
+        message_obj = Message(
+            notification=Notification(**serializer.data),
+            apns=APNSConfig(
+                headers={
+                    "apns-priority": "5",
+                },
+                payload=APNSPayload(
+                    aps=Aps(
+                        content_available=True,
+                    ),
+                ),
+            ),
+        )
         FCMDevice.objects.all().send_message(message_obj)
+
+
+class NotificationListAPIView(ListAPIView):
+    serializer_class = NotificationSerializer
+    queryset = NotificationMessage.objects.all()
+    permission_classes = [IsAdminUser]
