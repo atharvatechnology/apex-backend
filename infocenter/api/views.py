988561f872804afb8eda2e8baf821264c135b513
shovan777 <raj.shrestha778@gmail.com>
2022-11-14 14:56:58 +0545

    List the resource content in web api.

--- infocenter/api/views.py
+++ infocenter/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-from ..models import CourseInfo, CourseInfoCategory
+from ..models import CourseInfo, CourseInfoCategory, WebResouce
 from .serializers import (
     CourseInfoCategoryRetrieveSerializer,
     CourseInfoRetrieveSerializer,
+    WebResouceListSerializer,
 )
 
 
@@ -33,3 +34,10 @@ class CourseInfoRetrieveAPIView(RetrieveAPIView):
 
     serializer_class = CourseInfoRetrieveSerializer
     queryset = CourseInfo.objects.all()
+
+
+class WebResouceListAPIView(ListAPIView):
+    """WebResouce list view."""
+
+    serializer_class = WebResouceListSerializer
+    queryset = WebResouce.objects.all()
