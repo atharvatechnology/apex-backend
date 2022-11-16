ad12ff14f4a95a2adfe072ae3069ae74568418e2
sabina489 <Karkisabina869@gmail.com>
2022-11-16 15:41:17 +0545

    Add student_name and phone_number search fields.

--- counseling/api/serializers.py
+++ counseling/api/serializers.py
@@ -3,7 +3,52 @@ from rest_framework import serializers
 from counseling.models import Counseling
 
 
-class CounselingSerializer(serializers.ModelSerializer):
+class CounselingListSerializer(serializers.ModelSerializer):
+    """Serializer for Counseling."""
+
+    class Meta:
+        model = Counseling
+        fields = (
+            "id",
+            "student_name",
+            "counsellor",
+            "note",
+            "phone_number",
+            "date",
+        )
+
+
+class CounselingRetrieveSerializer(serializers.ModelSerializer):
+    """Serializer for Counseling."""
+
+    class Meta:
+        model = Counseling
+        fields = (
+            "id",
+            "student_name",
+            "counsellor",
+            "note",
+            "phone_number",
+            "date",
+        )
+
+
+class CounselingUpdateSerializer(serializers.ModelSerializer):
+    """Serializer for Counseling."""
+
+    class Meta:
+        model = Counseling
+        fields = (
+            "id",
+            "student_name",
+            "counsellor",
+            "note",
+            "phone_number",
+            "date",
+        )
+
+
+class CounselingDeleteSerializer(serializers.ModelSerializer):
     """Serializer for Counseling."""
 
     class Meta:
--- counseling/api/views.py
+++ counseling/api/views.py
@@ -1,9 +1,19 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import (
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from counseling.api.serializers import CounselingSerializer
+from counseling.api.serializers import (
+    CounselingDeleteSerializer,
+    CounselingListSerializer,
+    CounselingRetrieveSerializer,
+    CounselingUpdateSerializer,
+)
 from counseling.models import Counseling
 
 
@@ -11,7 +21,31 @@ class CounselingListAPIView(ListAPIView):
     """View for listing counseling."""
 
     permission_classes = [AllowAny]
-    serializer_class = CounselingSerializer
+    serializer_class = CounselingListSerializer
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ["student_name", "phone_number"]
+    queryset = Counseling.objects.all()
+
+
+class CounselingRetrieveAPIView(RetrieveAPIView):
+    """View for retrieving counseling."""
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = CounselingRetrieveSerializer
+    queryset = Counseling.objects.all()
+
+
+class CounselingUpdateAPIView(UpdateAPIView):
+    """View for updating counseling."""
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = CounselingUpdateSerializer
+    queryset = Counseling.objects.all()
+
+
+class CounselingDeleteAPIView(DestroyAPIView):
+    """View for deleting counseling."""
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = CounselingDeleteSerializer
     queryset = Counseling.objects.all()
