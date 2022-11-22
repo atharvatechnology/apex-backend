83e016c76d895ca4b8ac64128d869a33d9668591
sabina489 <Karkisabina869@gmail.com>
2022-11-22 14:32:31 +0545

    Add urls.

--- apex/urls.py
+++ apex/urls.py
@@ -69,6 +69,7 @@ api_admin_urls = [
     path("notifications/", include("notifications.api_admin.urls")),
     path("payments/", include("payments.api_admin.urls")),
     path("counseling/", include("counseling.api_admin.urls")),
+    path("bannerad/", include("bannerad.api_admin.urls")),
 ]
 
 fcm_urls = [
