928933933ab8c1ac8ffd7560284b14d03f84d687
sabina489 <Karkisabina869@gmail.com>
2022-11-22 14:30:50 +0545

    Create api_admin serailizer.

---
+++ bannerad/api_admin/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+
+from bannerad.models import BannerAd
+
+
+class BannerAdCreateSerializer(serializers.ModelSerializer):
+    """Serializer for creating bannerad."""
+
+    class Meta:
+        model = BannerAd
+        fields = (
+            "id",
+            "title",
+            "description",
+            "is_displayed",
+        )
+
+
+class BannerAdListSerializer(serializers.ModelSerializer):
+    """Serializer for listing bannerad."""
+
+    class Meta:
+        model = BannerAd
+        fields = (
+            "id",
+            "title",
+            "description",
+            "is_displayed",
+        )
+
+
+class BannerAdRetrieveSerializer(serializers.ModelSerializer):
+    """Serializer for retrieving bannerad."""
+
+    class Meta:
+        model = BannerAd
+        fields = (
+            "id",
+            "title",
+            "description",
+            "is_displayed",
+        )
+
+
+class BannerAdUpdateSerializer(serializers.ModelSerializer):
+    """Serializer for updating bannerad."""
+
+    class Meta:
+        model = BannerAd
+        fields = (
+            "id",
+            "title",
+            "description",
+            "is_displayed",
+        )
+
+
+class BannerAdDeleteSerializer(serializers.ModelSerializer):
+    """Serializer for deleting bannerad."""
+
+    class Meta:
+        model = BannerAd
+        fields = (
+            "id",
+            "title",
+            "description",
+            "is_displayed",
+        )
