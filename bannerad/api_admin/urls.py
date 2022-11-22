8efe6adaff283df67afd419d7ad6b0e6db8ceaaf
sabina489 <Karkisabina869@gmail.com>
2022-11-22 14:31:44 +0545

    CRUD operations for api_admin.

---
+++ bannerad/api_admin/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from bannerad.api_admin.views import (
+    BannerAdCreateAPIView,
+    BannerAdDeleteAPIView,
+    BannerAdListAPIView,
+    BannerAdRetrieveAPIView,
+    BannerAdUpdateAPIView,
+)
+
+urlpatterns = [
+    path("create/", BannerAdCreateAPIView.as_view(), name="bannerad-create"),
+    path("list/", BannerAdListAPIView.as_view(), name="bannerad-list"),
+    path(
+        "retrieve/<int:pk>/",
+        BannerAdRetrieveAPIView.as_view(),
+        name="bannerad-retrieve",
+    ),
+    path("update/<int:pk>/", BannerAdUpdateAPIView.as_view(), name="bannerad-update"),
+    path("delete/<int:pk>/", BannerAdDeleteAPIView.as_view(), name="bannerad-delete"),
+]
