87f02cfb63a8f44a4eec22409404e15e90a6d23e
sabina489 <Karkisabina869@gmail.com>
2022-11-29 11:27:36 +0545

    Create views for AdminTeacher

--- attendance/api_admin/views.py
+++ attendance/api_admin/views.py
@@ -3,17 +3,31 @@ from rest_framework import filters
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-from attendance.api_admin.serializers import StudentAttendanceAdminRetrieveSerializer
+from attendance.api_admin.serializers import (
+    StudentAttendanceAdminListSerializer,
+    TeacherAttendanceAdminListSerializer,
+)
 from attendance.filters import AttendanceFilter
-from attendance.models import StudentAttendance
+from attendance.models import StudentAttendance, TeacherAttendance
 
 
 class StudentAttendanceAdminListAPIView(ListAPIView):
     """View for listing admin student attendance."""
 
     permission_classes = [IsAuthenticated, IsAdminUser]
-    serializer_class = StudentAttendanceAdminRetrieveSerializer
+    serializer_class = StudentAttendanceAdminListSerializer
     queryset = StudentAttendance.objects.all()
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ["name"]
+    filterset_class = AttendanceFilter
+
+
+class TeacherAttendanceListAPIView(ListAPIView):
+    """View for listing admin teacher attendance."""
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = TeacherAttendanceAdminListSerializer
+    queryset = TeacherAttendance.objects.all()
+    filter_backends = [filters.SearchFilter]
     search_fields = ["name"]
     filterset_class = AttendanceFilter
