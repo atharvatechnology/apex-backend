da55acd38b4d28d47f9167818241f38b7f78755b
sabina489 <Karkisabina869@gmail.com>
2022-11-30 15:27:25 +0545

    Add urls for api_admin.

--- attendance/api_admin/urls.py
+++ attendance/api_admin/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from attendance.api_admin.views import (
     StudentAttendanceAdminListAPIView,
+    StudentAttendanceAdminRetrieveAPIView,
     TeacherAttendanceAdminListAPIView,
+    TeacherAttendanceAdminRetrieveAPIView,
 )
 
 urlpatterns = [
@@ -12,8 +14,18 @@ urlpatterns = [
         name="admin-student-list",
     ),
     path(
+        "student/retrieve/<int:pk>",
+        StudentAttendanceAdminRetrieveAPIView.as_view(),
+        name="admin-student-retrieve",
+    ),
+    path(
         "teacher/list/",
         TeacherAttendanceAdminListAPIView.as_view(),
         name="admin-teacher-list",
+    ),
+    path(
+        "teacher/retrieve/<int:pk>",
+        TeacherAttendanceAdminRetrieveAPIView.as_view(),
+        name="admin-teacher-retrieve",
     ),
 ]
