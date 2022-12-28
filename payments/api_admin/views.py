c8ade7ba2f6364dee412c7e7cf3c918135a5e9d3
Jabeen Shrestha <jabeenshrestha@gmail.com>
2022-12-28 10:50:17 +0545

    IsSuperAdminorDirector for payment list

--- payments/api_admin/views.py
+++ payments/api_admin/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from common.paginations import StandardResultsSetPagination
-from common.permissions import IsAccountant, IsAdminorSuperAdminorDirector
+from common.permissions import IsAccountant, IsSuperAdminorDirector
 from payments import PaymentStatus
 from payments.api_admin.filters import PaymentFilter
 from payments.api_admin.serializers import PaymentSerializer
@@ -13,9 +13,7 @@ from payments.models import Payment
 class PaymentListAPIView(ListAPIView):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.filter(status=PaymentStatus.PAID)
-    permission_classes = [
-        IsAuthenticated & (IsAdminorSuperAdminorDirector | IsAccountant)
-    ]
+    permission_classes = [IsAuthenticated & (IsSuperAdminorDirector | IsAccountant)]
     pagination_class = StandardResultsSetPagination
     filter_backends = [DjangoFilterBackend]
     filterset_class = PaymentFilter
