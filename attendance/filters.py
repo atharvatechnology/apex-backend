8a39560d7183f538a37dada2a0428c7315fcea1f
Jabeen Shrestha <jabeenshrestha@gmail.com>
2023-01-10 14:29:30 +0545

    date filter for attendances kwargs managed

--- attendance/filters.py
+++ attendance/filters.py
@@ -6,35 +6,22 @@ from attendance.models import Attendance, StudentAttendance, TeacherAttendance
 class AttendanceFilter(django_filters.FilterSet):
     """Filter for attendance."""
 
-    date_only = django_filters.DateFilter(field_name="date", lookup_expr="date")
-    date__gte = django_filters.DateFilter(field_name="date", lookup_expr="date__gte")
-    date__lte = django_filters.DateFilter(field_name="date", lookup_expr="date__lte")
-
     class Meta:
         model = Attendance
-        fields = ["date"]
-        # fields = {"date": ["exact", "gte", "lte"]}
+        fields = {"date": ["exact", "gte", "lte"]}
 
 
 class StudentAttendanceDateFilter(django_filters.FilterSet):
     """Filter for Student Attendance by date."""
 
-    date_only = django_filters.DateFilter(field_name="date", lookup_expr="date")
-    date__gte = django_filters.DateFilter(field_name="date", lookup_expr="date__gte")
-    date__lte = django_filters.DateFilter(field_name="date", lookup_expr="date__lte")
-
     class Meta:
         model = StudentAttendance
-        fields = ["date"]
+        fields = {"date": ["exact", "gte", "lte"]}
 
 
 class TeacherAttendanceDateFilter(django_filters.FilterSet):
     """Filter for Teacher Attendance by date."""
 
-    date_only = django_filters.DateFilter(field_name="date", lookup_expr="date")
-    date__gte = django_filters.DateFilter(field_name="date", lookup_expr="date__gte")
-    date__lte = django_filters.DateFilter(field_name="date", lookup_expr="date__lte")
-
     class Meta:
         model = TeacherAttendance
-        fields = ["date"]
+        fields = {"date": ["exact", "gte", "lte"]}
