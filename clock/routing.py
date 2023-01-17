9b8da282bec14ebd77e6377db476e6550489ce41
Sushil Khadka <punksusil.khadka@gmail.com>
2023-01-17 09:39:28 +0545

    staffing tracking with app integration and backend initiation
    websocket for liniking with mobile app
    admin can request for location using a single button.

--- clock/routing.py
+++ clock/routing.py
@@ -5,4 +5,7 @@ from . import consumers
 websocket_urlpatterns = [
     # re_path(r'ws/clock/', consumers.ClockConsumer.as_asgi()),
     re_path(r"ws/clock/(?P<room_name>\w+)/$", consumers.ClockConsumer.as_asgi()),
+    re_path(
+        r"ws/staff/(?P<room_name>\w+)/$", consumers.StaffTrackingConsumer.as_asgi()
+    ),
 ]
--- stafftracking/api_admin/views.py
+++ stafftracking/api_admin/views.py
@@ -1,9 +1,26 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from stafftracking.api_admin.serializers import StaffTrackingSerializer
 from stafftracking.models import StaffTracking
+
+channel_layer = get_channel_layer()
 
 
 class StaffTrackingListAPIView(ListAPIView):
     queryset = StaffTracking.objects.all()
     serializer_class = StaffTrackingSerializer
+
+
+class StaffTrackingRequestAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        user_id = self.kwargs.get("user_id")
+        async_to_sync(channel_layer.group_send)(
+            f"track_{user_id}",
+            {"type": "send_request_user", "status": "asdf"},
+        )
+
+        return Response({"msg": "Location has been requested."})
