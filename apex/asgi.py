2a51c610cd56b8fb20bf696622ea69b5999621b0
shovan777 <raj.shrestha778@gmail.com>
2023-01-28 11:00:12 +0545

    Fix asgi no reload.

--- apex/asgi.py
+++ apex/asgi.py
@@ -5,13 +5,18 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
+# flake8: noqa
 
 import os
+
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 
 import clock.routing
 
@@ -20,7 +25,7 @@ import clock.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apex.settings")
 
-django_asgi_app = get_asgi_application()
+# django_asgi_app = get_asgi_application()
 
 
 application = ProtocolTypeRouter(
