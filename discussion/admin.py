accdb606b069b9fccfb34f4f57c20853637dc435
SaRu-N <saruniroula73@gmail.com>
2023-02-07 14:33:41 +0545

    register discussion questions in admin

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from common.admin import CreatorBaseModelAdmin
+from discussion.models import Question 
 # Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(CreatorBaseModelAdmin):
+    list_display=[
+        'id',
+        'content',
+        'question',
+    ]
