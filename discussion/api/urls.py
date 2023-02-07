b0de6913e116a77ed545984d780d943c5809f21f
SaRu-N <saruniroula73@gmail.com>
2023-02-07 15:20:59 +0545

    url name update

--- discussion/api/urls.py
+++ discussion/api/urls.py
@@ -10,9 +10,9 @@ QuestionDestroyAPIView
 app_name = "discussion.api"
 
 urlpatterns = [
-    path('list/',QuestionListAPIView.as_view(),name='listquestion'),
-    path('create/',QuestionCreateAPIView.as_view(),name='createquestion'),
-    path('retrieve/<int:pk>/',QuestionRetrieveAPIView.as_view(),name='retrievequestion'),
-    path('update/<int:pk>/',QuestionUpdateAPIView.as_view(),name='updatequestion'),
-    path('delete/<int:pk>/',QuestionDestroyAPIView.as_view(),name='deletequestion'),
+    path('list/',QuestionListAPIView.as_view(),name='question-list'),
+    path('create/',QuestionCreateAPIView.as_view(),name='question-create'),
+    path('retrieve/<int:pk>/',QuestionRetrieveAPIView.as_view(),name='question-retrieve'),
+    path('update/<int:pk>/',QuestionUpdateAPIView.as_view(),name='question-update'),
+    path('delete/<int:pk>/',QuestionDestroyAPIView.as_view(),name='question-delete'),
 ]
