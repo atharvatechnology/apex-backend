29438fb3004b8d9620c2a33878a5144f5253fc55
SaRu-N <saruniroula73@gmail.com>
2023-02-07 13:09:42 +0545

    add permissions.

--- discussion/api/views.py
+++ discussion/api/views.py
@@ -15,26 +15,37 @@ from . serializers import (
     QuestionUpdateSerializer,
     QuestionCreateSerializer
 )
-
+from discussion.permissions import DiscussionPermission
 # Create your views here.
 
 class QuestionCreateAPIView(BaseCreatorCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionCreateSerializer
-    
+    permission_classes=[DiscussionPermission]
+
 class QuestionListAPIView(ListAPIView):
-    queryset = Question.objects.all()
     serializer_class = QuestionListSerializer
+    permission_classes=[DiscussionPermission]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_student:
+            return Question.objects.filter(created_by=user)
+        return Question.objects.all()
     
 class QuestionRetrieveAPIView(RetrieveAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionRetrieveSerializer
+    permission_classes=[DiscussionPermission]
 
 class QuestionUpdateAPIView(BaseCreatorUpdateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionUpdateSerializer
-    
+    permission_classes=[DiscussionPermission]
+
 class QuestionDestroyAPIView(DestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionRetrieveSerializer
+    permission_classes=[DiscussionPermission]
+
 
