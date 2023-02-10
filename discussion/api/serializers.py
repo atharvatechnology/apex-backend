51efc1b2d67d1175d9ea7a3d731b7fcba028a97a
SaRu-N <saruniroula73@gmail.com>
2023-02-10 14:53:07 +0545

    changes in format of the code

--- discussion/api/serializers.py
+++ discussion/api/serializers.py
@@ -1,53 +1,72 @@
 from rest_framework import serializers
-from discussion.models import  Question 
+
 from common.api.serializers import CreatorSerializer
+from discussion.models import Question
 
-"""Serializer to list all the questions"""
+
 class QuestionListSerializer(serializers.ModelSerializer):
-    replies ="QuestionListSerializer(many=True,read_only=True)"
-    question ="QuestionListSerializer(read_only=True)"
+    """Serializer to list all the questions."""
+
+    replies = "QuestionListSerializer(many=True,read_only=True)"
+    question = "QuestionListSerializer(read_only=True)"
+
     class Meta:
         ref_name = "Discussion Question"
-        model=Question
-        fields=['id','content','question','replies','created_by','created_at']
+        model = Question
+        fields = ["id", "content", "question", "replies", "created_by", "created_at"]
 
-"""Serializer to retrieve a question"""
+
 class QuestionRetrieveSerializer(serializers.ModelSerializer):
-    replies =QuestionListSerializer(many=True,read_only=True)
-    question =QuestionListSerializer(read_only=True)
+    """Serializer to retrieve a question."""
+
+    replies = QuestionListSerializer(many=True, read_only=True)
+    question = QuestionListSerializer(read_only=True)
+
     class Meta:
         ref_name = "Discussion Question"
-        model=Question
-        fields=['id','content','question','replies','created_by','updated_by','created_at','updated_at']
+        model = Question
+        fields = [
+            "id",
+            "content",
+            "question",
+            "replies",
+            "created_by",
+            "updated_by",
+            "created_at",
+            "updated_at",
+        ]
 
-"""Serializer to create question"""
+
 class QuestionCreateSerializer(CreatorSerializer):
-    class Meta:
-         ref_name = "Discussion Question"
-         model=Question
-         fields=['content','question']
-    def create(self, validated_data):
-        question= validated_data.get("question")
-        if (question is not None) and question.is_question==False:
-                raise serializers.ValidationError(
-                    "Reply for Answer is not Possible",
-                ) 
-        else:
-            return Question.objects.create(**validated_data) 
+    """Serializer to create question."""
 
-"""Serializer to update question"""
-class QuestionUpdateSerializer(CreatorSerializer):
     class Meta:
-         ref_name = "Discussion Question"
-         model=Question
-         fields=['content']
+        ref_name = "Discussion Question"
+        model = Question
+        fields = ["content", "question"]
+
+    def create(self, validated_data):
+        question = validated_data.get("question")
+        if (question is not None) and question.is_question is False:
+            raise serializers.ValidationError(
+                "Reply for Answer is not Possible",
+            )
+        else:
+            return Question.objects.create(**validated_data)
+
+
+class QuestionUpdateSerializer(CreatorSerializer):
+    """Serializer to update question."""
+
+    class Meta:
+        ref_name = "Discussion Question"
+        model = Question
+        fields = ["content"]
+
     def update(self, instance, validated_data):
-        if (instance.question is not None) and instance.question.is_question==False:
-                raise serializers.ValidationError(
-                    "Reply for Answer is not Possible",
-                )
+        if (instance.question is not None) and instance.question.is_question is False:
+            raise serializers.ValidationError(
+                "Reply for Answer is not Possible",
+            )
         else:
             return super().update(instance, validated_data)
-
-            
-
--- discussion/api_admin/serializers.py
+++ discussion/api_admin/serializers.py
@@ -1,55 +1,72 @@
 from rest_framework import serializers
-from discussion.models import  Question 
+
 from common.api.serializers import CreatorSerializer
-from accounts.api_admin.serializers import UserMiniAdminSerializer
+from discussion.models import Question
 
-"""Serializer to list all the questions"""
+
 class QuestionAdminListSerializer(serializers.ModelSerializer):
-    replies ="QuestionAdminListSerializer(many=True,read_only=True)"
-    question ="QuestionAdminListSerializer(read_only=True)"
-    
+    """Serializer to list all the questions."""
+
+    replies = "QuestionAdminListSerializer(many=True,read_only=True)"
+    question = "QuestionAdminListSerializer(read_only=True)"
+
     class Meta:
         ref_name = "Discussion Question"
-        model=Question
-        fields=['id','content','question','replies','created_by','created_at']
+        model = Question
+        fields = ["id", "content", "question", "replies", "created_by", "created_at"]
 
-"""Serializer to retrieve a question"""
+
 class QuestionAdminRetrieveSerializer(serializers.ModelSerializer):
-    replies =QuestionAdminListSerializer(many=True,read_only=True)
-    question =QuestionAdminListSerializer(read_only=True)
+    """Serializer to retrieve a question."""
+
+    replies = QuestionAdminListSerializer(many=True, read_only=True)
+    question = QuestionAdminListSerializer(read_only=True)
+
     class Meta:
         ref_name = "Discussion Question"
-        model=Question
-        fields=['id','content','question','replies','created_by','updated_by','created_at','updated_at']
+        model = Question
+        fields = [
+            "id",
+            "content",
+            "question",
+            "replies",
+            "created_by",
+            "updated_by",
+            "created_at",
+            "updated_at",
+        ]
 
-"""Serializer to create question"""
+
 class QuestionAdminCreateSerializer(CreatorSerializer):
-    class Meta:
-         ref_name = "Discussion Question"
-         model=Question
-         fields=['content','question']
-    def create(self, validated_data):
-        question= validated_data.get("question")
-        if (question is not None) and question.is_question==False:
-                raise serializers.ValidationError(
-                    "Reply for Answer is not Possible",
-                ) 
-        else:
-            return Question.objects.create(**validated_data) 
+    """Serializer to create question."""
 
-"""Serializer to update question"""
-class QuestionAdminUpdateSerializer(CreatorSerializer):
     class Meta:
-         ref_name = "Discussion Question"
-         model=Question
-         fields=['content']
+        ref_name = "Discussion Question"
+        model = Question
+        fields = ["content", "question"]
+
+    def create(self, validated_data):
+        question = validated_data.get("question")
+        if question is not None and question.is_question is False:
+            raise serializers.ValidationError(
+                "Reply for Answer is not Possible",
+            )
+        else:
+            return Question.objects.create(**validated_data)
+
+
+class QuestionAdminUpdateSerializer(CreatorSerializer):
+    """Serializer to update question."""
+
+    class Meta:
+        ref_name = "Discussion Question"
+        model = Question
+        fields = ["content"]
+
     def update(self, instance, validated_data):
-        if (instance.question is not None) and instance.question.is_question==False:
-                raise serializers.ValidationError(
-                    "Reply for Answer is not Possible",
-                )
+        if (instance.question is not None) and instance.question.is_question is False:
+            raise serializers.ValidationError(
+                "Reply for Answer is not Possible",
+            )
         else:
             return super().update(instance, validated_data)
-
-            
-
