1e4b66b54616ad90296bd1ee24df4f3b68df84a4
shovan777 <raj.shrestha778@gmail.com>
2023-02-12 17:33:15 +0545

    Remove redundant get_object in update override.

--- enrollments/api/views.py
+++ enrollments/api/views.py
@@ -167,7 +167,19 @@ class ExamEnrollmentUpdateAPIView(UpdateAPIView):
                 {"detail": "Your answers have already been submitted."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        return super().update(request, *args, **kwargs)
+        instance = exam_enrollment
+        partial = kwargs.pop("partial", False)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+        # return super().update(request, *args, **kwargs)
 
 
 class ExamEnrollmentRetrieveAPIView(RetrieveAPIView):
