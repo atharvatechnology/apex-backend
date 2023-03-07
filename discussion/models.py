afe1d77c9ab0096d3ab6258220f6fdee1e4f4743
SaRu-N <saruniroula73@gmail.com>
2023-03-07 15:45:23 +0545

    ordering by updated date.

--- discussion/models.py
+++ discussion/models.py
@@ -1,23 +1,36 @@
 from django.db import models
+from django.utils import timezone
+
 from common.models import CreatorBaseModel
+
 # Create your models here.
 
+
 class Question(CreatorBaseModel):
-    content=models.TextField()
-    question = models.ForeignKey('self' , null=True , blank=True , on_delete=models.CASCADE,related_name='replies')
+    content = models.TextField()
+    question = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE, related_name="replies"
+    )
 
     class Meta:
-        ordering= ['-created_at']
-    
+        ordering = ["-updated_at", "-created_at"]
+
     def __str__(self):
         return self.content
-    
+
+    def reply_count(self):
+        return self.replies.count()
+
     @property
     def is_question(self):
         if self.question is None:
             return True
         return False
-    # @property
-    # def answers(self):
-    #     return Question.objects.filter(question=self).reverse()
 
+    def save(self, *args, **kwargs):
+        if (self.question) and self.created_by.is_student:
+            question = self.question
+            question.updated_at = timezone.now()
+            question.updated_by = self.created_by
+            question.save()
+        super().save(*args, **kwargs)
