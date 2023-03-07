ed2d7f314f939bbe686e33e8ba1fdf36543224e2
SaRu-N <saruniroula73@gmail.com>
2023-03-07 15:39:30 +0545

    add api view for replies.

--- discussion/api_admin/urls.py
+++ discussion/api_admin/urls.py
@@ -1,18 +1,37 @@
 from django.urls import path
-from .views import(
-QuestionAdminListAPIView,
-QuestionAdminCreateAPIView,
-QuestionAdminRetrieveAPIView,
-QuestionAdminUpdateAPIView,
-QuestionAdminDestroyAPIView
+
+from .views import (
+    QuestionAdminCreateAPIView,
+    QuestionAdminDestroyAPIView,
+    QuestionAdminListAPIView,
+    QuestionAdminRetrieveAPIView,
+    QuestionAdminUpdateAPIView,
+    QuestionRepliesListAdminAPIView,
 )
 
 app_name = "discussion.api_admin"
 
 urlpatterns = [
-    path('list/',QuestionAdminListAPIView.as_view(),name='admin-question-list'),
-    path('create/',QuestionAdminCreateAPIView.as_view(),name='admin-question-create'),
-    path('retrieve/<int:pk>/',QuestionAdminRetrieveAPIView.as_view(),name='admin-question-retrieve'),
-    path('update/<int:pk>/',QuestionAdminUpdateAPIView.as_view(),name='admin-question-update'),
-    path('delete/<int:pk>/',QuestionAdminDestroyAPIView.as_view(),name='admin-question-delete'),
+    path("list/", QuestionAdminListAPIView.as_view(), name="admin-question-list"),
+    path(
+        "<int:pk>/replies/",
+        QuestionRepliesListAdminAPIView.as_view(),
+        name="admin-replies-list",
+    ),
+    path("create/", QuestionAdminCreateAPIView.as_view(), name="admin-question-create"),
+    path(
+        "retrieve/<int:pk>/",
+        QuestionAdminRetrieveAPIView.as_view(),
+        name="admin-question-retrieve",
+    ),
+    path(
+        "update/<int:pk>/",
+        QuestionAdminUpdateAPIView.as_view(),
+        name="admin-question-update",
+    ),
+    path(
+        "delete/<int:pk>/",
+        QuestionAdminDestroyAPIView.as_view(),
+        name="admin-question-delete",
+    ),
 ]
--- discussion/api_admin/views.py
+++ discussion/api_admin/views.py
@@ -1,46 +1,58 @@
-from rest_framework.generics import(
-ListAPIView,
-RetrieveAPIView,
-DestroyAPIView,
-)
-from common.api.views import (
-    BaseCreatorCreateAPIView,
-    BaseCreatorUpdateAPIView
-)
+from rest_framework.generics import DestroyAPIView, ListAPIView, RetrieveAPIView
+
+from common.api.views import BaseCreatorCreateAPIView, BaseCreatorUpdateAPIView
+from common.paginations import StandardResultsSetPagination
 from common.permissions import IsAdminOrSuperAdminOrDirector
 from discussion.models import Question
 
-from . serializers import (
+from .serializers import (
+    QuestionAdminCreateSerializer,
     QuestionAdminListSerializer,
     QuestionAdminRetrieveSerializer,
     QuestionAdminUpdateSerializer,
-    QuestionAdminCreateSerializer
 )
-# Create your views here.
 
+
+# Create your views here.
 class QuestionAdminCreateAPIView(BaseCreatorCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionAdminCreateSerializer
-    permission_classes=[IsAdminOrSuperAdminOrDirector]
+    permission_classes = [IsAdminOrSuperAdminOrDirector]
+
 
 class QuestionAdminListAPIView(ListAPIView):
-    queryset = Question.objects.all()
+    queryset = Question.objects.filter(question=None)
     serializer_class = QuestionAdminListSerializer
-    permission_classes=[IsAdminOrSuperAdminOrDirector]
-    
+    permission_classes = [IsAdminOrSuperAdminOrDirector]
+    pagination_class = StandardResultsSetPagination
+
+
+class QuestionRepliesListAdminAPIView(ListAPIView):
+    """View to retrieve replies for a specific question."""
+
+    serializer_class = QuestionAdminListSerializer
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        question_id = self.kwargs["pk"]
+        question = Question.objects.get(id=question_id)
+        return question.replies.all()
+
+
 class QuestionAdminRetrieveAPIView(RetrieveAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionAdminRetrieveSerializer
-    permission_classes=[IsAdminOrSuperAdminOrDirector]
+    permission_classes = [IsAdminOrSuperAdminOrDirector]
+    pagination_class = StandardResultsSetPagination
+
 
 class QuestionAdminUpdateAPIView(BaseCreatorUpdateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionAdminUpdateSerializer
-    permission_classes=[IsAdminOrSuperAdminOrDirector]
+    permission_classes = [IsAdminOrSuperAdminOrDirector]
+
 
 class QuestionAdminDestroyAPIView(DestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionAdminRetrieveSerializer
-    permission_classes=[IsAdminOrSuperAdminOrDirector]
-
-
+    permission_classes = [IsAdminOrSuperAdminOrDirector]
