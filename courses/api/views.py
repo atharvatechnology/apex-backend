95c4f4fa30e19eecc316a9adcec4168034ae8245
shovan777 <raj.shrestha778@gmail.com>
2023-03-28 15:15:31 +0545

    Restrict access to course through inactive sessions.

--- courses/api/views.py
+++ courses/api/views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
 from common.api.mixin import InterestWiseOrderMixin, PublishableModelMixin
 from common.paginations import StandardResultsSetPagination
@@ -14,6 +15,7 @@ from courses.api.serializers import (
 )
 from courses.filters import CourseFilter
 from courses.models import Course, CourseCategory
+from enrollments.models import CourseThroughEnrollment, Enrollment, SessionStatus
 
 
 class CourseListAPIView(PublishableModelMixin, InterestWiseOrderMixin, ListAPIView):
@@ -41,6 +43,40 @@ class CourseRetrieveAPIAfterEnrollView(PublishableModelMixin, RetrieveAPIView):
     permission_classes = [IsAuthenticated, IsCourseEnrolledActive]
     serializer_class = CourseRetrieveSerializerAfterEnroll
     queryset = Course.objects.all()
+
+    def _get_student_enrollment(self, obj):
+        if not hasattr(self, "_student_enrollment"):
+            try:
+                student_enrollment = obj.enrolls.filter(
+                    student=self.request.user
+                ).latest("id")
+            except Enrollment.DoesNotExist:
+                student_enrollment = None
+            self._student_enrollment = student_enrollment
+        return self._student_enrollment
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        student_enrollment = self._get_student_enrollment(instance)
+        if student_enrollment:
+            try:
+                student_through_enrollment = (
+                    student_enrollment.course_enrolls.select_related(
+                        "selected_session"
+                    ).latest("id")
+                )
+            except CourseThroughEnrollment.DoesNotExist:
+                student_through_enrollment = None
+            request.student_through_enrollment = student_through_enrollment
+            session = student_through_enrollment.selected_session
+            session_status = session.status
+            if session_status == SessionStatus.ACTIVE:
+                serializer = self.get_serializer(instance)
+                return Response(serializer.data)
+            return Response({"detail": "Course Session is not active"}, status=400)
+        return Response(
+            f"You do not have any active sessions for {instance.name} course"
+        )
 
 
 class CourseRetrieveAPIBeforeEnrollView(PublishableModelMixin, RetrieveAPIView):
