16b93ba3e668306eaa4e89b1f315f8601275218c
Sushil Khadka <punksusil.khadka@gmail.com>
2023-04-24 13:40:37 +0545

    order of notification by created_at

--- notifications/models.py
+++ notifications/models.py
@@ -5,3 +5,6 @@ class NotificationMessage(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
